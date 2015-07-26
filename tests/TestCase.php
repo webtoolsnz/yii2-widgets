89fd690aa313f462d7cde8e28f2705b3993c7045
Byron Adams <byron@webtools.co.nz>
2015-07-26 13:13:52 +1200

    Adding currency input tests

---
+++ tests/TestCase.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace tests;
+
+use yii\helpers\ArrayHelper;
+use yii\web\AssetManager;
+use yii\web\View;
+
+/**
+ * This is the base class for all tests
+ */
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    public static $params;
+
+    /**
+     * Mock application prior running tests.
+     */
+    protected function setUp()
+    {
+        $this->mockWebApplication(
+            [
+                'components' => [
+                    'request' => [
+                        'class' => 'yii\web\Request',
+                        'url' => '/test',
+                        'enableCsrfValidation' => false,
+                    ],
+                    'response' => [
+                        'class' => 'yii\web\Response',
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Clean up after test.
+     * By default the application created with [[mockApplication]] will be destroyed.
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->destroyApplication();
+    }
+
+    protected function mockApplication($config = [], $appClass = '\yii\console\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'test-app',
+            'basePath' => __DIR__,
+            'vendorPath' => $this->getVendorPath(),
+        ], $config));
+    }
+
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'test-app',
+            'basePath' => __DIR__,
+            'vendorPath' => $this->getVendorPath(),
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => '123',
+                    'scriptFile' => __DIR__ .'/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+                'assetManager' => [
+                    'basePath' => '@tests/data/assets',
+                    'baseUrl' => '/',
+                ]
+            ]
+        ], $config));
+    }
+
+    protected function getVendorPath()
+    {
+        return dirname(dirname(__DIR__)) . '/vendor';
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        \Yii::$app = null;
+    }
+
+    /**
+     * Asserting two strings equality ignoring line endings
+     *
+     * @param string $expected
+     * @param string $actual
+     */
+    public function assertEqualsWithoutLE($expected, $actual)
+    {
+        $expected = str_replace("\r\n", "\n", $expected);
+        $actual = str_replace("\r\n", "\n", $actual);
+        $this->assertEquals($expected, $actual);
+    }
+
+}
