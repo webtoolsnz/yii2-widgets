f31e4ee89460707ec9a5bd207ecf0a27663d5ffa
Byron Adams <byron@webtools.co.nz>
2015-07-26 13:13:52 +1200

    Adding currency input tests

---
+++ tests/CurrencyInputTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace tests;
+
+use webtoolsnz\widgets\CurrencyInput;
+use yii\web\View;
+
+class CurrencyInputTest extends TestCase
+{
+    public function testRenderWithNameAndId()
+    {
+        $out = CurrencyInput::widget([
+            'id' => 'test',
+            'name' => 'test-widget-name',
+            'value' => '500',
+        ]);
+
+        $expected = '<div class="input-group"><div class="input-group-addon">$</div><input type="number" id="test" class="form-control" name="test-widget-name" value="500" step="0.01" min="0"></div>';
+        $this->assertEqualsWithoutLE($expected, $out);
+    }
+
+    public function testRenderWithOptions()
+    {
+        $out = CurrencyInput::widget([
+            'id' => 'test',
+            'name' => 'test-widget-name',
+            'value' => '100',
+            'options' => [
+                'step' => 10,
+                'class' => 'foo',
+                'min' => 20,
+            ]
+        ]);
+
+        $expected = '<div class="input-group"><div class="input-group-addon">$</div><input type="number" id="test" class="foo form-control" name="test-widget-name" value="100" step="10" min="20"></div>';
+        $this->assertEqualsWithoutLE($expected, $out);
+    }
+
+
+
+}
