0e9d7bbfabf1fb24f0f1d50abcb8d5a044e9108a
Bruce Aldridge <bruce@incode.co.nz>
2015-08-24 11:49:35 +1200

    adding checkbox button groups

---
+++ src/CheckboxButtonGroupAsset.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @link https://bitbucket.org/webtoolsnz/yii2-widgets
+ * @copyright Copyright (c) 2015 Webtools Ltd
+ */
+
+namespace webtoolsnz\widgets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class CheckboxButtonGroupAsset
+ * @package webtoolsnz\widgets
+ */
+class CheckboxButtonGroupAsset extends AssetBundle
+{
+    public $js = [
+        'js/checkbox-button-group.js',
+    ];
+
+    public $css = [
+        'css/radio-button-group.css'
+    ];
+
+    public $depends = [
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/assets';
+        parent::init();
+    }
+}
